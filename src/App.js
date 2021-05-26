ca0f4d7c33a1453eafd1b29499274b89b766cd33
Paulina Torres <iptorresb25@gmail.com>
2021-05-26 17:33:01 -0600

    adding Badge

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react"
 import Layout from "./pages/layout/Layout"
 import Main from "./pages/main/Main"
+import Badge from "./components/Badge"
 
 function App() {
   return (
     <React.Fragment>
     <Layout>
-      <Main></Main>
+      <Badge profile_picture={"https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}></Badge>
     </Layout>
     </React.Fragment>
   );
