894bfe9b6606a69282fdfc940b39225555e5e676
Paulina Torres <iptorresb25@gmail.com>
2021-05-26 19:00:15 -0600

    adding badge form

---
+++ src/components/BadgeForm.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from "react"
+import './style/BadgeForm.css'
+
+class BadgeForm extends Component{
+    render(){
+        return(
+            <React.Fragment>
+                <h1>Badge Form</h1>
+            </React.Fragment>
+        );
+    }
+}
+
+export default BadgeForm
--- src/pages/NewBadge/NewBadge.jsx
+++ src/pages/NewBadge/NewBadge.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../../components/Hero";
 import "./NewBadge.css";
 import Badge from "../../components/Badge"
+import BadgeForm from "../../components/BadgeForm"
 
 class NewBadge extends React.Component{
 
@@ -29,11 +30,16 @@ class NewBadge extends React.Component{
                         <Badge
                         header_picture = {this.state.form.header_picture || "https://www.universetoday.com/wp-content/uploads/2021/04/speckle.jpg"}
                         profile_picture = {this.state.form.profile_picture || "https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}
-                        
+                        name = {this.state.form.name||"John Ibalp  "}
+                        age = {this.state.form.age || "23"}
+                        city = {this.state.form.city || "Salt Lake, Ut"}
+                        followers = {this.state.form.followers || "0"}
+                        likes = {this.state.form.likes || "0"}
+                        posts = {this.state.form.posts || "0"}
                         ></Badge>
                     </div>
                     <div className="col">
-
+                        <BadgeForm></BadgeForm>
                     </div>
                 </div>
             </React.Fragment>
