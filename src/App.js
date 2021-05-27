365afa6475ae7012064d0f3c27453e22d24d897d
Paulina Torres <iptorresb25@gmail.com>
2021-05-26 22:49:31 -0600

    Adding error 404

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Main from "./pages/main/Main"
 import NewBadge from "./pages/NewBadge/NewBadge"
 import LogIn from "./pages/LogIn/LogIn"
 import SignIn from "./pages/SignIn/SignIn"
+import Error404 from "./pages/Error/error"
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route exact path="/new" component={NewBadge}></Route>
         <Route exact path="/login" component={LogIn}></Route>
         <Route exact path="/signin" component={SignIn}></Route>
+        <Route component={Error404}></Route>
       </Switch>
     </Layout>
     </BrowserRouter>
---
+++ src/pages/Error/error.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import "./error.css"
+import errorImg from "../../images/error.jpg"
+
+var error404 =()=>{
+    return(
+        <React.Fragment>
+            <img src={errorImg} alt="" />
+        </React.Fragment>
+    )
+}
+
+export default error404
