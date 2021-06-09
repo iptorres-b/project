998840a0bd239ed88fe3a05a0c5d911d5a3de9bf
Paulina Torres <iptorresb25@gmail.com>
2021-06-08 22:56:55 -0600

    adding everything

---
+++ src/pages/Badges/Badges.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "./Badges.css"
+
+class Badges extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+                <h1>All Badges</h1>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Badges;
