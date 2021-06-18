1dd2dc4bbbc4c795b742be27b2b888ea261234c5
Paulina Torres <iptorresb25@gmail.com>
2021-06-18 13:06:52 -0600

    path changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = props => {
             <div className="Hero">
                 <div className="Hero__color_filter" style={{height: props.h}}></div>
                 <div className="Hero__img" style={{height: props.h}}>
-                    <img src={HeroImage} alt="" />
+                    <img src="https://images.pexels.com/photos/4456224/pexels-photo-4456224.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" alt="" />
                 </div>
             </div>
         </React.Fragment>
